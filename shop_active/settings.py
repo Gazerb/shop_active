ab351085c4f8855a6e495aa52f9cc41d90e497a3
Gazerb <>
2023-06-04 18:16:17 +0000

    crispy forms package installed and added to settings

--- shop_active/settings.py
+++ shop_active/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'shop_active.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
