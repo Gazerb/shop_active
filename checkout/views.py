6c360749829b0f78d6529dfd41db4905f2890fcc
Gazerb <>
2023-06-05 18:02:31 +0000

    stripe setup added in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.SRTIPE_PUBLIC_KEY
+    srtipe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your shopping bag at the moment")
@@ -17,12 +20,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    stripe.PaymentIntent.create(
+        ammount=stripe_total,
+        currency=settings.stripe_currency,
+    )
+
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing. Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NFbmhCm58OOqFRHaUxs7pIT97HKzAMufTZMYSVqPB0T7EeuwLQFdJjFqc4bcgorsNfFWX52d57G7gaCvrivl6q600LyB388N4',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
