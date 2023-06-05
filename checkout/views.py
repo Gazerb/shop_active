2d684ca66d788eb34c2b3bce569d92af18bf15ff
Gazerb <>
2023-06-05 18:26:54 +0000

    tested functionality of card payment all working as expected

--- checkout/views.py
+++ checkout/views.py
@@ -9,27 +9,28 @@ import stripe
 
 
 def checkout(request):
-    stripe_public_key = settings.SRTIPE_PUBLIC_KEY
-    srtipe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, "There's nothing in your shopping bag at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
-    stripe.PaymentIntent.create(
-        ammount=stripe_total,
-        currency=settings.stripe_currency,
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
     )
 
     order_form = OrderForm()
-    
+
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key missing. Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
