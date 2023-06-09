409a3830a138e784a6da2ac4f6f2a12abbfa46d4
Gazerb <>
2023-06-09 18:23:59 +0000

    changes made to checkout to complete stripe payment, currently has issues which i will be looking to fix, commiting work so far

--- shop_active/settings.py
+++ shop_active/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'shop_active.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
