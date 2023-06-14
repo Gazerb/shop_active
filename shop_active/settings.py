8efe19111ffa75e596230cd3bbccf331b87e4f8d
Gazerb <>
2023-06-14 12:42:54 +0000

    changed settings so secret key is taken from the environment and debug to true if development is in environment

--- shop_active/settings.py
+++ shop_active/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qc)iho^#__rvwczusaxg(qmak8g)utfotbbo$n^98!&d)s4-le'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shop-active.herokuapp.com', '8000-gazerb-shop-active-s45o09gd5w.us2.codeanyapp.com']
 
