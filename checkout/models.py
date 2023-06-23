4b627d40bf1aea5916bb4cfe386c5b499352c784
Gazerb <>
2023-06-23 12:24:09 +0000

    code tidy up to remove pip8 errors

--- checkout/models.py
+++ checkout/models.py
@@ -14,8 +14,8 @@ class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(
             UserProfile, on_delete=models.SET_NULL,
-            null=True, 
-            blank=True, 
+            null=True,
+            blank=True,
             related_name='orders'
     )
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -30,27 +30,27 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
             max_digits=6,
-            decimal_places=2, 
-            null=False, 
+            decimal_places=2,
+            null=False,
             default=0,
     )
     order_total = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        null=False, 
+        max_digits=10,
+        decimal_places=2,
+        null=False,
         default=0,
     )
     grand_total = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        null=False, 
+        max_digits=10,
+        decimal_places=2,
+        null=False,
         default=0,
     )
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(
-        max_length=254, 
-        null=False, 
-        blank=False, 
+        max_length=254,
+        null=False,
+        blank=False,
         default=''
     )
 
@@ -65,9 +65,12 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total')
+        )['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * \
+                                settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -88,29 +91,29 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-        Order, 
-        null=False, 
-        blank=False, 
-        on_delete=models.CASCADE, 
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
         related_name='lineitems'
     )
     product = models.ForeignKey(
-        Product, 
-        null=False, 
-        blank=False, 
+        Product,
+        null=False,
+        blank=False,
         on_delete=models.CASCADE
     )
     product_size = models.CharField(
-        max_length=2, 
-        null=True, 
+        max_length=2,
+        null=True,
         blank=True
-    ) # XS, S, M, L, XL,XXL
+    )
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-        max_digits=6, 
-        decimal_places=2, 
-        null=False, 
-        blank=False, 
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
         editable=False
     )
 
